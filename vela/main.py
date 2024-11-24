90620f1825a6c7a304f9b7b5c01a0ad91c805dbb
AJ Fischer Jr <agejfischer@gmail.com>
2024-11-24 17:04:50 -0500

    Add basic testing of light sensor

--- vela/main.py
+++ vela/main.py
@@ -1,5 +1,15 @@
+import time
+import board
+import adafruit_bh1750
+
+
 def main():
-    pass
+    i2c = board.I2C()
+    sensor = adafruit_bh1750.BH1750(i2c)
+
+    while True:
+        print("%.2f Lux" % sensor.lux)
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
