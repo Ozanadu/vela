f79d57eb829dcc1b1523275c8f8a98d9896360bd
AJ Fischer Jr <agejfischer@gmail.com>
2024-11-24 21:31:56 -0500

    Add lux and timestamp logging to csv

--- vela/main.py
+++ vela/main.py
@@ -1,3 +1,5 @@
+import csv
+import datetime
 import time
 import board
 import adafruit_bh1750
@@ -8,7 +10,13 @@ def main():
     sensor = adafruit_bh1750.BH1750(i2c)
 
     while True:
-        print("%.2f Lux" % sensor.lux)
+        currentTime = datetime.datetime.now()
+        luxReading = ('%.2f Lux' % sensor.lux)
+
+        with open('./data/lux.csv', 'a+') as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerow(currentTime, luxReading)
+        
         time.sleep(1)
 
 if __name__ == "__main__":
